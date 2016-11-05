c775671222a3529487b37bf1ab480ee018b4db5e
Cedric Van Goethem <github@zepheus.net>
2016-11-05 11:41:27 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,53 @@
+# ESP8266 433/315Mhz MQTT over WiFi bridge
+
+This MQTT bridge allows you to control cheap home-automation RF power outlets using a common MQTT interface.
+With e.g. [Home-assistant](https://github.com/home-assistant/home-assistant), these power outlets can then be switched on/off remotely over the internet and integrate seamlessly with IFTT.
+
+## MQTT auto-discovery
+The MQTT broker is automatically discovered through mDNS. When you do not want to use this, simply put the static IP or hostname of your MQTT broker in the `hostString` variable.
+
+To enable MQTT discovery on the broker, simply install `avahi-daemon`. For a Raspberry Pi, use the following command:
+```bash
+sudo apt-get install avahi-daemon
+```
+Next, create the following service declaration in `/etc/avahi/services/mqtt.service`:
+```xml
+<service-group>
+  <name replace-wildcards="yes">MQTT on %h</name>
+  <service>
+    <type>_mqtt._tcp</type>
+    <port>1883</port>
+  </service>
+</service-group>
+```
+Restart your avahi-daemon service: `sudo service avahi-daemon restart`.
+
+_Please note that discovery over mDNS only works within the same multicast/broadcast domain and cannot cross network interfaces, unless a mDNS-repeater or igmp-proxy is used._
+
+## MQTT protocol
+The following MQTT payload should be sent:
+- **Topic**: `/switch/rf/[ID]`
+    - e.g. `/switch/rf/5`
+- **Payload**: `[protocol]|[pulselength]|[binary code]` 
+    - e.g. `4|101|101010100100101100101100`
+
+These protocol, pulselength and binary variables can be sniffed as described in [Obtaining RF codes](#obtaining-rf-codes).
+
+For persistency, the _retain-flag_ can be set such that the codes are re-sent when the ESP8266 is rebooted.
+
+## Code setup
+
+* Get the Arduino ESP8266 firmware. For install instructions please check [here](https://github.com/esp8266/Arduino).
+* Install the Arduino [RC-switch](https://github.com/sui77/rc-switch) library
+* Install the Arduino [PubSubClient](https://github.com/knolleary/pubsubclient) library
+* Configure your wireless password in the `WIFI_SSID` and `WIFI_PASSWORD` variables
+
+## Wiring
+The firmware was tested on a NodeMCU development board.
+The 433/315 transmitter data pin was connected to D2 pin (GPIO4), but any other gpio can be used. Most transmitters can be powered using 3.3V directly.
+
+## Obtaining RF codes
+The RF codes can be sniffed using the [RC-switch examples](https://github.com/sui77/rc-switch/tree/master/examples/ReceiveDemo_Advanced) using an Arduino or ESP8266 as well. Keep in mind that there might be a difference measured in pulselength depending on the device, since rc-switch (currently) does not adjust for clock speed. It is therefore advisable to also use an ESP8266 when sniffing the codes.
+
+## License
+The code falls under the MIT license.
---
+++ bridge.ino
@@ -0,0 +1,156 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ESP8266mDNS.h>
+#include <RCSwitch.h>
+
+#include <stdint.h>
+
+#define WIFI_SSID "..."
+#define WIFI_PASSWORD "..."
+
+#define MQTT_TOPIC "switch/rf/+"
+
+#define TX_PIN 4 // Receiver on GPIO4 / D2.
+#define TX_REPEAT 15
+
+RCSwitch mySwitch = RCSwitch();
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+char hostString[16];
+IPAddress brokerIp;
+uint16_t brokerPort;
+
+void setup() {
+  Serial.begin(115200);
+  sprintf(hostString, "ESP_%06X", ESP.getChipId());
+
+  /* Setup transmitter */
+  mySwitch.enableTransmit(TX_PIN);
+  mySwitch.setRepeatTransmit(TX_REPEAT);
+  
+  delay(10);
+  setup_wifi();
+  if(!find_raspberry()){
+    // reboot?
+  }
+  Serial.print("IP: ");
+  Serial.print(brokerIp);
+  Serial.printf(", Port: %d\n", brokerPort);
+  client.setServer(brokerIp, brokerPort);
+  client.setCallback(callback);
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  uint8_t protocol;
+  uint16_t pulselen;
+  char* code;
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // In future handle out of bounds check on topic (strcmp + len)
+  int switchNr = atoi((const char*)(topic + sizeof(MQTT_TOPIC) - 2)); //-2, 1 for null-termination, 1 for wildcard +
+  Serial.printf("Switch: %d\n", switchNr);
+
+  // Parse using strtok in format `protocol|pulselen|bincode`
+  char *tmp = (char*)payload;
+  char* tok = strtok(tmp, "|");
+  if(tok){
+    protocol = atoi(tok);
+    tok = strtok(NULL, "|");
+    if(tok && protocol <= 5){
+      pulselen = atoi(tok);
+      tok = strtok(NULL, "|");
+      if(tok){
+        code = tok;
+        
+        Serial.printf("prot: %u, pulselen: %u, code: %s\n", protocol, pulselen, code);
+        mySwitch.setPulseLength(pulselen);
+        mySwitch.setProtocol(protocol);
+        mySwitch.send(code);
+      }
+    }
+  }
+}
+
+
+/* Discover raspberry using MDNS */
+boolean find_raspberry() {
+  if (!MDNS.begin(hostString)) {
+    Serial.println("Error setting up MDNS responder!");
+    return false;
+  } else {
+    Serial.println("MDNS responder started...");
+    delay(10);
+
+    /* Start discovery of MDNS (e.g. avahi) service advertisements for MQTT */
+    int n = -1;
+    do {
+      if(!n) {
+        delay(1000);
+        Serial.println("Did not find MQTT broker, retrying...");
+      }
+      n = MDNS.queryService("mqtt", "tcp");
+    } while(n == 0);
+    Serial.println("MDNS broker found!");
+
+    brokerIp = MDNS.IP(0);
+    brokerPort = MDNS.port(0);
+    return true;
+  }
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.hostname(hostString);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // If you do not want to use a username and password, change next line to
+    // if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Subscribe to the topic
+      client.subscribe(MQTT_TOPIC);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
+
